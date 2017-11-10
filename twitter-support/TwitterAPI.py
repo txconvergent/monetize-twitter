c19edeb3868ce965a4e162a14e6b330c1e5091c2
NimayRKumar <33074769+NimayRKumar@users.noreply.github.com>
2017-11-10 11:32:30 -0600

    Add files via upload
    Small tests for getting user input, and accessing TimeLine of relevant company account

--- twitter-support/TwitterAPI.py
+++ twitter-support/TwitterAPI.py
@@ -13,7 +13,7 @@ access_token_key = '928484335031914496-dpXe6Lk6iRShm80XzR3GMnl1hOorhk7'
 access_token_secret = 'ji94aVMvSCKBlEneis3xIMInmQkrjaCfgWT1OWMbBbRc6'
 
 '''
-Twitter maintains a Python repository that with full API 
+Twitter maintains a Python repository that with full API
 functionality; it's probably our best option.
 You download it from command line with:
     pip install python-twitter
@@ -33,7 +33,7 @@ api = twitter.Api(consumer_key=consumer_key,
 '''
 The API is pretty dense for documentation, so here's
 a brief overview of the GetSearch method's parameters.
-They are all optional parameters, so they must be 
+They are all optional parameters, so they must be
 entered in the form "term='my iphone sucks wtf'"
     term - the search term
     geolocation - lat,long,radius i.e. geocode="37.781157,-122.398720,1mi"
@@ -54,18 +54,58 @@ harder to find API for. For a twitter.Status object called tweet, we have:
     tweet.id - used for identifying tweets
     tweet.in_reply_to_screen_name - profile this tweet is replying to
     tweet.in_reply_to_status_id - id of tweet replying to (YESSS THIS WILL BE USEFUL)
+
+Other (potentially) useful methods:
+    GetUserTimeline: directly access a specific user's timeline
+        api.GetUserTimeline(screen_name="Username")
+    GetFfriends: not as useful, but access a user's friends list
 '''
 def smallTest():
     print("Small Scale Test for AppleSupport")
     for tweet in api.GetSearch(term="applesupport", count=5):
         print("-- Tweet -- ")
-        print("Username: @" + tweet.user.screen_name) 
+        print("Username: @" + tweet.user.screen_name)
         print(tweet.fulltext if hasattr(tweet, 'fulltext') else tweet.text)
         print("Retweets: " + str(tweet.retweet_count))
         print("Favorties: " + str(tweet.favorite_count))
         print("Hashtags: " + ", ".join([ht.text for ht in tweet.hashtags]))
         print("")
 
-# main method in python, temp for testing 
+
+#simple function tests getting search input and separates words
+#company name is first element of list returned
+def getInput():
+    search = input("Please enter a search in the format @<username> <string>\n").split(' ')
+    search[0] = search[0].replace('@', '')
+    return search
+
+
+
+
+#function to parse tweets and assigns a quantified 'relevance' to each tweet
+#unfinished
+def searchTimeLine(search):
+
+    statuses = api.GetUserTimeline(screen_name=search[0])
+
+    '''Perhaps formulize relevance based on keywords matched, retweets, and replies
+            perhaps: a*keyword_count + b*(retweets/10) + c*(other_factors)
+       Then return list of tweets in relevant order
+       next function can determine threads from those tweets
+       finally print out
+    '''
+
+    print([s.text for s in statuses])
+
+
+
+
+
+
+
+
+# main method in python, temp for testing
 if __name__ == '__main__':
     smallTest()
+    search = getInput()
+    searchTimeLine(search)
